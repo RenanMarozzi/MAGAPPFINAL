f76b2d15b84071875399bb1d5b314fdb12fb0a75
Rafael-Oliveira1 <rafinha121.severino@gmail.com>
2020-11-06 02:21:38 -0300

    Resolvendo Bugs e acertando rotas

--- src/app/cadastro/cadastro.page.ts
+++ src/app/cadastro/cadastro.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { UsuariosService } from '../services/usuarios.service';
+import { ToastController } from '@ionic/angular';
+
 
 interface Usuario {
   Nome: String;
@@ -37,7 +40,8 @@ export class CadastroPage implements OnInit {
 
   usuario: Usuario;
 
-  constructor(private usuariosService: UsuariosService) { }
+  constructor(private usuariosService: UsuariosService, private router: Router, public toastController: ToastController) { }
+
 
   ngOnInit() {
 
@@ -48,8 +52,16 @@ export class CadastroPage implements OnInit {
       Nome: this.Nome, DataNascimento :this.DataNascimento, Sexo: this.Sexo, EstadoCivil: this.EstadoCivil, Endereco: this.Endereco, Telefone: this.Telefone, DtBatismo: this.DtBatismo, NvlTreinamento: this.NvlTreinamento, SaberImportante: this.SaberImportante, Email: this.Email, Senha:this.Senha
       }
     this.usuariosService.CadastraUsuario( this.usuario );
-    
+    this.router.navigate(['/home']);
+    this.presentToast('Usuario Cadastrado com Sucesso');
 
   }
 
+  async presentToast(Mensagem) {
+    const toast = await this.toastController.create({
+      message: Mensagem, 
+      duration: 2000
+    });
+    toast.present();
+  }
 }
